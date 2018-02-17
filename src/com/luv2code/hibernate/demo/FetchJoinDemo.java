76a248eb3b212ab9bf1ea21c0d608afde18d2c97
Terry Davis <davist15@email.franklin.edu>
2018-02-16 21:21:35 -0500

    Initial Commit

---
+++ src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,69 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .addAnnotatedClass(Course.class)
+								 .buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// start a transaction
+			session.beginTransaction();
+			
+			
+			// get the instructor from db
+			int theId=1;
+			
+			Query<Instructor> query =
+					session.createQuery("select i from Instructor i "
+										+ "JOIN FETCH i.courses "
+										+ "where i.id=:theInstructorId",							
+							Instructor.class);
+			
+			// set parameter on query
+			query.setParameter("theInstructorId", theId);
+			
+			// execute query and get instructor
+			Instructor tempInstructor = query.getSingleResult();
+			
+			System.out.println("LUV2CODE: Instructor: " + tempInstructor);
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			System.out.println("\nLUV2CODE: The session is now closed!\n");
+			
+			// get courses for the instructor
+			System.out.println("LUV2CODE: Courses: " + tempInstructor.getCourses());
+			
+			System.out.println("LUV2CODE: Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
